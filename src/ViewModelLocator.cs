82cb0087b0f42fbd0ff0a1a47bf1df07e39eb9df
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-08-15 02:47:10 +0100

    Created viewModel locator class. Added TestAPP

---
+++ src/ViewModelLocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace XamarinViewModelLocator {
+    public static class ViewModelLocator {
+        public static readonly BindableProperty AutoWireViewModelProperty =
+           BindableProperty.CreateAttached("AutoWireViewModel", typeof(bool), 
+               typeof(ViewModelLocator), default(bool), 
+               propertyChanged: OnAutoWireViewModelChanged);
+
+        private static void OnAutoWireViewModelChanged(BindableObject bindable, 
+                    object oldValue, object newValue) {
+            var view = bindable as Element;
+            if (view == null) {
+                return;
+            }
+
+            var viewType = view.GetType();
+            var viewName = viewType.FullName.Replace(".Views.", ".ViewModels.");
+            var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+            var viewModelName = string.Format(CultureInfo.InvariantCulture, "{0}ViewModel, {1}",
+                viewName, viewAssemblyName);
+
+            var viewModelType = Type.GetType(viewModelName);
+            if (viewModelType == null) {
+                return;
+            }
+            var viewModel = Activator.CreateInstance(viewModelType);
+           // var viewModel = _container.Resolve(viewModelType);
+            view.BindingContext = viewModel;
+        }
+
+        public static bool GetAutoWireViewModel(BindableObject bindable) {
+            return (bool)bindable.GetValue(AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(BindableObject bindable, bool value) {
+            bindable.SetValue(AutoWireViewModelProperty, value);
+        }
+    }
+}
